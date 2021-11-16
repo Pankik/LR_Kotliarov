a94acf0f03e258284bb75a8c1eb2fcd5fb9baa9b
Pankik <76562208+Pankik@users.noreply.github.com>
2021-11-16 11:32:43 +0300

    Update login.php

--- LR2/Cookie/login.php
+++ LR2/Cookie/login.php
@@ -7,7 +7,7 @@ if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !="")
 }
 else 
 {
-   header("Location:/Verholetov/index.php");
+   header("Location:/Kotliarov/index.php");
 } 
 session_write_close();
 ?>
