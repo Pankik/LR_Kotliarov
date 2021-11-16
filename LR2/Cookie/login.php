9851fada988a5fbbbaf363dea1dc98592af657f9
Pankik <76562208+Pankik@users.noreply.github.com>
2021-11-16 11:31:04 +0300

    Update login.php

--- LR2/Cookie/login.php
+++ LR2/Cookie/login.php
@@ -1,27 +1,21 @@
 <?
-if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !="")
+session_start();
+if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !="") 
 {
-	$user_email=$_POST["email"];
-	$user_pass=$_POST["pass"];
-	setcookie("email",$user_email,time()+3600);
-	setcookie("pass",$user_pass,time()+3600);
-	header("Location:/Kotliarov/login.php");
-	
-
+  $_SESSION['email'] = $_POST["email"];
+  $_SESSION['pass'] = $_POST["pass"];
 }
-
-
-
+else 
+{
+   header("Location:/Verholetov/index.php");
+} 
+session_write_close();
 ?>
-
-<form action="/Kotliarov/" method="GET" >
-<input name="logout" type="hidden" value="yes"></input>
-	<button type="submit" class="btn btn-primary mb-3">Logout</button>
- </form>
- 
- 
+<head></head>
 <body>
-<h1> <?php echo"Welcome, ".$_COOKIE['email']."!"; ?> </h1>
+<h1>Log IN <?=$_POST["email"]?></h1>
+<div>
+<a href="/Verholetov/index.php?logout=yes">Выход</a>
+</div>
 </body>
-
 
