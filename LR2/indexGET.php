017c17d0297b0ea607631a0f9d24735ae637f969
Pankik <76562208+Pankik@users.noreply.github.com>
2021-11-16 10:35:26 +0300

    Update indexGET.php

--- LR2/indexGET.php
+++ LR2/indexGET.php
@@ -1,27 +1,21 @@
-<form action="/sdfds/" method="GET">
-<div class = "mb-3">
-<label for="exampleColorInput" class="form-label">Color picker</label>
-<input name="iv_color" type="color" class="form-control form-control-color" id="exampleColorInput" value="#563d7c" title="Choose your color"
-value= "#ffffff" title= "Choose your color">
-</div>
-<button type="submit" class="btn btn-ptimary">Submit</button>
-</form>
-
 <?
-
 $bg_color = "#fff";
 if (isset($_GET["iv_color"]))
 	$bg_color = $_GET["iv_color"];
 else
-	$bg_color = "#fff";
-	
+	$bg_color = "#fff";	
 ?>
-
 <head>
 </head>
 <body style ="background-color: <?=$bg_color?>">
+<form action="/Kotliarov/index.php" method="GET">
+<div class="mb-3">
+  <label for="exampleColorInput" class="form-label">Color picker</label>
+  <input name="iv_color" type="color" class="form-control-color" id="exampleColorInput" value = "#ffffff" title = "Chose your color">
+</div>
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
 <div class = "container">
 <h1><?
 echo "Выбери любимый цвет"?></h1>
-
 </body>
