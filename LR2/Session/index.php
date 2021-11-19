bdd65ee4d47a77574ad2433f655717ec99c440cd
Pankik <76562208+Pankik@users.noreply.github.com>
2021-11-19 08:58:22 +0300

    Update index.php

--- LR2/Session/index.php
+++ LR2/Session/index.php
@@ -1,3 +1,20 @@
+<?
+session_start();
+$_SESSION = array();
+if($_GET["logout"]=='yes')
+{   if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+}
+session_destroy();
+
+?>
+<head></head>
+<body>
 <form action="/Kotliarov/login.php" method="POST">
 <div class="mb-3" style="margin-bottom: 10px; margin-top:10px">
   <label for="exampleFormControlInput1" class="form-label">Login</label>
@@ -8,15 +25,8 @@
     <input name="pass" type="password" class="form-control" id="inputPassword2" placeholder="Password">
   </div>
   <div class="col-auto">
-<button type="submit" class="btn btn-primary mb-3">Confirm</button>
-
-
-
+    <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
   </div>
-  
-	
- </form>
-
-<body>
-
+  </form>
+  </body>
 
