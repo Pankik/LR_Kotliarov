55ec57e44f0945d20da81c96ec984e3c80e703cc
Pankik <76562208+Pankik@users.noreply.github.com>
2021-11-19 08:59:10 +0300

    Update login.php

--- LR2/Session/login.php
+++ LR2/Session/login.php
@@ -1,34 +1,22 @@
 
-<?php
+<?
 session_start();
-
-if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !="")
+if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !="") 
 {
-	$user_email=$_POST["email"];
-	$user_pass=$_POST["pass"];
-	setcookie("email",$user_email,time()+3600);
-	setcookie("pass",$user_pass,time()+3600);
-	header("Location:/Kotliarov/login.php");
-	
-
+  $_SESSION['email'] = $_POST["email"];
+  $_SESSION['pass'] = $_POST["pass"];
 }
-if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !="")
+else 
 {
-	$_SESSION["email"]=$_POST["email"];
-	$_SESSION["pass"]=$_POST["pass"];
-	
-}
-
+   header("Location:/Kotliarov/index.php");
+} 
+session_write_close();
 ?>
-
-<form action="/Kotliarov/" method="GET" >
-<input name="logout" type="hidden" value="yes"></input>
-	<button type="submit" class="btn btn-primary mb-3">Logout</button>
- </form>
- 
- 
+<head></head>
 <body>
-<h1> <?php echo"Welcome, ".$_SESSION['email']."!"; ?> </h1>
+<h1>Log IN <?=$_POST["email"]?></h1>
+<div>
+<a href="/Verholetov/index.php?logout=yes">Выход</a>
+</div>
 </body>
-
 
