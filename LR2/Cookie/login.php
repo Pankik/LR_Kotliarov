6b15bc32448a42970c7da9e4d6049eafa3be8f0b
Pankik <76562208+Pankik@users.noreply.github.com>
2021-11-19 10:00:15 +0300

    Update login.php

--- LR2/Cookie/login.php
+++ LR2/Cookie/login.php
@@ -1,9 +1,11 @@
 <?
-session_start();
-if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !="" && $_POST["email"] !="") 
+if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !="" && $_POST["email"] !="")
 {
-  $_SESSION['email'] = $_POST["email"];
-  $_SESSION['pass'] = $_POST["pass"];
+	$user_email=$_POST["email"];
+	$user_pass=$_POST["pass"];
+	setcookie("email",$user_email,time()+3600);
+	setcookie("pass",$user_pass,time()+3600);
+	
 }
 else 
 {
