92e885479287a41b5ff717c9612116da09dbaf4b
Pankik <76562208+Pankik@users.noreply.github.com>
2021-11-23 11:38:01 +0300

    Update login.php

--- LR2/Session/login.php
+++ LR2/Session/login.php
@@ -1,17 +1,26 @@
-
 <?
+
 session_start();
-if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !="" && $_POST["email"] !="") 
+if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !=""&& $_POST["email"] !="") 
 {
   $_SESSION['email'] = $_POST["email"];
   $_SESSION['pass'] = $_POST["pass"];
 }
 else 
 {
-   header("Location:/Kotliarov/index.php");
+	if(isset($_SESSION['email'])&& isset($_SESSION['pass'])&& $_SESSION['email']!=""&& $_SESSION['pass']!="")
+	{
+		$_POST["email"]=$_SESSION['email'];
+        $_POST["pass"]=$_SESSION['pass'];
+	}
+	else
+	{
+		header("Location:/Kotliarov/index.php");
+	}
 } 
 session_write_close();
 ?>
+<html>
 <head></head>
 <body>
 <h1>Log IN <?=$_POST["email"]?></h1>
@@ -19,4 +28,4 @@ session_write_close();
 <a href="/Kotliarov/index.php?logout=yes">Выход</a>
 </div>
 </body>
-
+</html>
